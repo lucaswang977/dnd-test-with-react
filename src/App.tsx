fc0e2e4c956bd781c487e5d1f014a8c4bad5f003
lucaswang <118956159+lucaswang977@users.noreply.github.com>
2022-12-27 17:30:59 +0900

    Rewrite most of the code.
    - Restore to the previous usable status.
    - Components are splitted into list and note.
    - Drag is okay, but drop has not been done yet.

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,29 @@
 // https://codepen.io/lucaswang977/pen/QWxeGem?editors=0011
 
 // TODO:
-// 1. Create Collection component to hold the vertical list of items.
+// 1. Create Collection component to hold a vertical list of Notes.
+// 2. Collection has a state array of every note, includes their height.
+// 3. Should we save all the rect info or just save the refs in the state? Lists' refs in Grid, Notes' refs in List.
+// 4. OnMouseMove(after mousedown), we first check if the center point of the Note is enter a list via Lists' refs state.
+// 5. Then we will check the Notes' height in this List via the Notes' refs state.
+// 6. We don't have to refresh the refs when window is resizing.
 
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 
-type Rect = { x: number; y: number; h: number; w: number };
-
-const isPosInRect = (pos: { x: number; y: number }, rect: Rect) =>
+const isPosInRect = (pos: { x: number; y: number }, rect: DOMRect) =>
   pos.x >= rect.x &&
-  pos.x <= rect.x + rect.w &&
+  pos.x <= rect.x + rect.width &&
   pos.y >= rect.y &&
-  pos.y <= rect.y + rect.h;
+  pos.y <= rect.y + rect.height;
 
 type GridData = {
   activeItem?: {
     col: number;
     row: number;
-    x: number;
-    y: number;
+    mouseDownX: number;
+    mouseDownY: number;
     dx: number;
     dy: number;
-    rectX: number;
-    rectY: number;
   };
   grid: Note[][];
 };
@@ -32,118 +33,36 @@ type Note = {
   text: string;
 };
 
-const initialGridData: GridData = {
-  grid: [
-    [
-      {
-        id: 0,
-        text: 'dummy text of the printing and typesetting industry. Lorem Ipsum has been the',
-      },
-      {
-        id: 1,
-        text: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using',
-      },
-      {
-        id: 2,
-        text: 'There are many variations of passages of Lorem Ipsum available, but the majority have suffered',
-      },
-    ],
-    [
-      {
-        id: 3,
-        text: 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. ',
-      },
-      {
-        id: 4,
-        text: 'There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour ',
-      },
-      {
-        id: 5,
-        text: 'The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form',
-      },
-      {
-        id: 6,
-        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam',
-      },
-      {
-        id: 7,
-        text: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae',
-      },
-    ],
-    [
-      {
-        id: 8,
-        text: 'But I must explain to you how all this mistaken idea of denouncing',
-      },
-      {
-        id: 9,
-        text: 'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati ',
-      },
-      {
-        id: 10,
-        text: 'On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment',
-      },
-      {
-        id: 11,
-        text: 'be welcomed and every pain avoided. But in certain circumstances and owing to the claims of duty or the obligations of business it will frequently occur that',
-      },
-    ],
-  ],
-};
+interface ListInterface {
+  listId: number,
+  data: Note[],
+  saveRef: (element: HTMLElement | null) => void;
+  selectedNoteIndex: number | undefined,
+  selectedNotePos: { x: number, y: number } | undefined,
+  onNoteSelected: (ev: React.MouseEvent<HTMLDivElement, MouseEvent>, activeItem: { col: number; row: number }) => void
+}
 
-const useMousePosition = () => {
-  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
-  useEffect(() => {
-    const updateMousePosition = (ev: MouseEvent) => {
-      setMousePosition({ x: ev.clientX, y: ev.clientY });
-    };
-    window.addEventListener('mousemove', updateMousePosition);
-    return () => {
-      window.removeEventListener('mousemove', updateMousePosition);
-    };
-  }, []);
-  return mousePosition;
-};
-
-const List = (props: { id: string; data: Note[] }) => {
-  const handleMouseDown = (
-    ev: React.MouseEvent<HTMLDivElement, MouseEvent>,
-    activeItem: { col: number; row: number }
-  ) => {
-    const rect = (ev.currentTarget as HTMLDivElement).getBoundingClientRect();
-    console.log('MouseDown', ev.pageX, ev.pageY, rect);
-
-    setGridState((gs) => {
-      return {
-        ...gs,
-        activeItem: {
-          col: activeItem.col,
-          row: activeItem.row,
-          x: ev.pageX,
-          y: ev.pageY,
-          dx: ev.pageX - rect.x,
-          dy: ev.pageY - rect.y,
-          rectX: rect.x,
-          rectY: rect.y,
-        },
-      };
-    });
-  };
-
+const List = (props: ListInterface) => {
   return (
-    <div id={props.id} className="items">
-      {props.data.map((item, rowId) => {
+    <div ref={props.saveRef} className="list">
+      {props.data.map((note, rowId) => {
+        let transformStyle = {};
+        if (props.selectedNoteIndex !== undefined && props.selectedNotePos !== undefined && props.selectedNoteIndex === rowId) {
+          transformStyle = {
+            transform: `translateX(${props.selectedNotePos.x}px) translateY(${props.selectedNotePos.y}px) scale(1.05)`,
+          };
+        }
         return (
           <div
             key={rowId + 1}
+            className="note"
             style={transformStyle}
-            className="item"
             onMouseDown={(ev) =>
-              handleMouseDown(ev, { col: colIndex, row: rowIndex })
+              props.onNoteSelected(ev, { col: props.listId, row: rowId })
             }
           >
-            <p>Item {row.id + 1}</p>
-            <p className="text">{row.text}</p>
+            <p>Item {rowId + 1}</p>
+            <p className="text">{note.text}</p>
           </div>
         );
       })}
@@ -151,31 +70,48 @@ const List = (props: { id: string; data: Note[] }) => {
   );
 };
 
-const Grid = () => {
-  const [gridState, setGridState] = useState(initialGridData);
+const Grid = (props: { gridData: GridData }) => {
+  const [gridState, setGridState] = useState(props.gridData);
+  const listRefs = useRef<HTMLElement[]>([]);
 
   // It will be accessed in window's event handler
-  const mousePos = useMousePosition();
+  const [mousePos, _setMousePos] = useState<{ x: number, y: number }>();
+  const mousePosRef = useRef(mousePos);
+  const setMousePos = (pos: { x: number, y: number }) => {
+    mousePosRef.current = mousePos;
+    _setMousePos(pos);
+  }
 
   useEffect(() => {
     if (!gridState.activeItem || !mousePos) return;
 
     setGridState((gs) => {
+      if (!gs.activeItem) return gs;
+      // TODO: Calculate which list the selected note is on right now.
+      if (listRefs.current) {
+        listRefs.current.map((item, index) => {
+          if (isPosInRect(mousePos, item.getBoundingClientRect())) {
+            console.log('** Move in list: ', index);
+          }
+        })
+      }
+
       return {
         ...gs,
         activeItem: {
           ...gs.activeItem!,
-          x: mousePos.x,
-          y: mousePos.y,
+          dx: mousePos.x - gs.activeItem.mouseDownX,
+          dy: mousePos.y - gs.activeItem.mouseDownY,
         },
       };
     });
   }, [mousePos]);
 
   const handleMouseUp = (ev: MouseEvent) => {
-    console.log('MouseUp', ev.pageX, ev.pageY);
+    console.log('MouseUp', ev.clientX, ev.clientY);
 
     window.removeEventListener('mouseup', handleMouseUp);
+    window.removeEventListener('mousemove', handleMouseMove);
 
     setGridState((gs) => {
       return {
@@ -184,58 +120,57 @@ const Grid = () => {
     });
   };
 
-  const handleMouseEnter = (
-    ev: React.MouseEvent<HTMLDivElement, MouseEvent>
+  const handleMouseMove = (ev: MouseEvent) => {
+    setMousePos({ x: ev.clientX, y: ev.clientY });
+  }
+
+  const handleMouseDown = (
+    ev: React.MouseEvent<HTMLDivElement, MouseEvent>,
+    activeItem: { col: number; row: number }
   ) => {
-    const gs = gridState;
-    if (!gs.activeItem) return;
+    console.log('MouseDown', ev.clientX, ev.clientY, activeItem);
 
-    const element = ev.currentTarget as HTMLDivElement;
-    const colId: number = +element.id;
+    window.addEventListener('mouseup', handleMouseUp);
+    window.addEventListener('mousemove', handleMouseMove);
 
-    console.log('MouseEnter: ', colId, gs.activeItem.col);
+    setGridState((gs) => {
+      return {
+        ...gs,
+        activeItem: {
+          col: activeItem.col,
+          row: activeItem.row,
+          mouseDownX: ev.clientX,
+          mouseDownY: ev.clientY,
+          dx: 0,
+          dy: 0,
+        },
+      };
+    });
   };
 
   return (
-    <div className="wrapper">
+    <div className="grid-wrapper">
       {gridState.grid.map((column, colIndex) => {
-        const oneList = column.map((row, rowIndex) => {
-          let transformStyle = {};
-          if (
-            gridState.activeItem &&
-            gridState.activeItem.col === colIndex &&
-            gridState.activeItem.row === rowIndex
-          ) {
-            const x =
-              gridState.activeItem.x -
-              gridState.activeItem.dx -
-              gridState.activeItem.rectX;
-            const y =
-              gridState.activeItem.y -
-              gridState.activeItem.dy -
-              gridState.activeItem.rectY;
-            transformStyle = {
-              transform: `translate(${x}px, ${y}px) scale(1.05)`,
-            };
-          }
-          return (
-            <div
-              key={row.id + 1}
-              style={transformStyle}
-              className="item"
-              onMouseDown={(ev) =>
-                handleMouseDown(ev, { col: colIndex, row: rowIndex })
-              }
-            >
-              <p>Item {row.id + 1}</p>
-              <p className="text">{row.text}</p>
-            </div>
-          );
-        });
+        let selectedNoteIndex = undefined;
+        let selectedNotePos = undefined;
+        if (gridState.activeItem && gridState.activeItem.col == colIndex) {
+          selectedNoteIndex = gridState.activeItem.row;
+
+          const x = gridState.activeItem.dx
+          const y = gridState.activeItem.dy;
+          selectedNotePos = { x: x, y: y };
+        }
+        const saveRef = (element: HTMLElement | null) => {
+          if (listRefs.current && element)
+            listRefs.current[colIndex] = element;
+          console.log(listRefs.current);
+        }
+
         return (
-          <div key={colIndex} id={colIndex.toString()} className="items">
-            {oneList}
-          </div>
+          <List saveRef={saveRef} key={colIndex} listId={colIndex} data={column}
+            onNoteSelected={handleMouseDown}
+            selectedNoteIndex={selectedNoteIndex}
+            selectedNotePos={selectedNotePos} />
         );
       })}
     </div>
@@ -243,10 +178,69 @@ const Grid = () => {
 };
 
 function App() {
+  const initialGridData: GridData = {
+    grid: [
+      [
+        {
+          id: 0,
+          text: 'dummy text of the printing and typesetting industry. Lorem Ipsum has been the',
+        },
+        {
+          id: 1,
+          text: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using',
+        },
+        {
+          id: 2,
+          text: 'There are many variations of passages of Lorem Ipsum available, but the majority have suffered',
+        },
+      ],
+      [
+        {
+          id: 3,
+          text: 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. ',
+        },
+        {
+          id: 4,
+          text: 'There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour ',
+        },
+        {
+          id: 5,
+          text: 'The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form',
+        },
+        {
+          id: 6,
+          text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam',
+        },
+        {
+          id: 7,
+          text: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae',
+        },
+      ],
+      [
+        {
+          id: 8,
+          text: 'But I must explain to you how all this mistaken idea of denouncing',
+        },
+        {
+          id: 9,
+          text: 'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati ',
+        },
+        {
+          id: 10,
+          text: 'On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment',
+        },
+        {
+          id: 11,
+          text: 'be welcomed and every pain avoided. But in certain circumstances and owing to the claims of duty or the obligations of business it will frequently occur that',
+        },
+      ],
+    ],
+  };
+
   return (
     <div className="app">
       <h1 className="title">Drag &amp; Drop Grid Layout in React</h1>
-      <Grid />
+      <Grid gridData={initialGridData} />
     </div>
   );
 }
